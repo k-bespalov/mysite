6f8c27fed2e9fbcfc059ecee4f81a3db1beced9e
konstantin <kos5tya@yandex.ru>
2017-04-03 23:12:49 +0300

    add likes/dislikes counter

--- MoneyCounterSite/views.py
+++ MoneyCounterSite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from django.http import JsonResponse
 from django.contrib import auth
-from MoneyCounterSite.models import Party, Profile, User
+from MoneyCounterSite.models import Party, Profile, User, Like
 
 def party_list(request):
     user = auth.get_user(request).username
@@ -45,6 +45,8 @@ def party_detail(request, party_id):
                 'name': party.name,
                 'datetime': party.datetime,
                 'place': party.place,
+                'likes': like_dislike_counter(2, party_id)[0],
+                'dislikes': like_dislike_counter(2, party_id)[1],
                 'persons': [ {'id': person.id, 'first_name': User.objects.get(id=person.user_id).first_name,
                             'last_name': User.objects.get(id=person.user_id).last_name} for person in Profile.objects.filter(party=party_id)]
     })
@@ -72,9 +74,22 @@ def show_profile(request, id):
             'username': user.username,
             'first_name': user.first_name,
             'last_name': user.last_name,
-            'telephone_number': profile.telephone_number
+            'telephone_number': profile.telephone_number,
+            'likes': like_dislike_counter(3, id)[0],
+            'dislikes': like_dislike_counter(3, id)[1],
         }
     )
+
+def like_dislike_counter(content, object):
+    counter = [0, 0]
+    list = Like.objects.filter(content_type=content, object_id=object).values_list('positive', flat=True)
+    for flag in list:
+        if flag:
+            counter[0] += 1
+        else:
+            counter[1] += 1
+    return counter
+
 
 
 
